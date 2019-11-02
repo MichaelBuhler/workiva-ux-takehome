ebc2d6b164c053101c23f5e11086e8971dcb991d
Michael Buhler <michael@michaelbuhler.com>
2019-11-01 21:34:53 -0400

    convert: Content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import Content from "./components/Content";
 
 export default class App extends React.Component {
     render () {
         return (
-            <Header/>
+            <div>
+                <Header/>
+                <Content/>
+            </div>
         );
     }
 }
---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Content.css'
+
+export default class Content extends React.Component {
+    render () {
+        return (
+            <div id="content" className="wrapper"/>
+        );
+    }
+}
